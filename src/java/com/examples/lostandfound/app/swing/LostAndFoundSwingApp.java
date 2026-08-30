51c417095e52e44c3de40815effdda8f14cc392d
myrkmr <mayoorhablani@gmail.com>
2026-08-30 20:20:51 +0200

    launcher and load startup items

--- src/java/com/examples/lostandfound/app/swing/LostAndFoundSwingApp.java
+++ src/java/com/examples/lostandfound/app/swing/LostAndFoundSwingApp.java
@@ -1,10 +1,20 @@
 package com.examples.lostandfound.app.swing;
 
+import java.awt.EventQueue;
+import java.util.concurrent.Callable;
+
+import com.examples.lostandfound.controller.LostAndFoundController;
+import com.examples.lostandfound.repository.mongo.LostItemMongoRepository;
+import com.examples.lostandfound.view.swing.LostItemSwingView;
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
 @Command(mixinStandardHelpOptions = true)
-public class LostAndFoundSwingApp {
+public class LostAndFoundSwingApp implements Callable<Void> {
 
 	@Option(names = "--mongo-host")
 	private String mongoHost = "localhost";
@@ -17,4 +27,26 @@ public class LostAndFoundSwingApp {
 
 	@Option(names = "--db-collection")
 	private String collectionName = "lostitems";
+
+	public static void main(String[] args) {
+		new CommandLine(new LostAndFoundSwingApp()).execute(args);
+	}
+
+	@Override
+	public Void call() {
+		EventQueue.invokeLater(() -> {
+			LostItemMongoRepository lostItemRepository = new LostItemMongoRepository(
+					new MongoClient(new ServerAddress(mongoHost, mongoPort)),
+					databaseName,
+					collectionName);
+			LostItemSwingView lostItemView = new LostItemSwingView();
+			LostAndFoundController lostAndFoundController = new LostAndFoundController(
+					lostItemRepository,
+					lostItemView);
+			lostItemView.setLostAndFoundController(lostAndFoundController);
+			lostItemView.setVisible(true);
+			lostAndFoundController.allLostItems();
+		});
+		return null;
+	}
 }
