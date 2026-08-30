11e43c3d742f4ad7d95a00c4baeaaf991cef9bd1
myrkmr <mayoorhablani@gmail.com>
2026-08-30 15:58:57 +0200

    failing test for launcher configuration

--- src/it/com/examples/lostandfound/app/swing/LostAndFoundSwingAppIT.java
+++ src/it/com/examples/lostandfound/app/swing/LostAndFoundSwingAppIT.java
@@ -13,4 +13,17 @@ public class LostAndFoundSwingAppIT {
 		int exitCode = new CommandLine(new LostAndFoundSwingApp()).execute("--help");
 		assertThat(exitCode).isZero();
 	}
+
+	@Test
+	public void testMongoConfigurationOptionsAreAccepted() {
+		CommandLine.ParseResult parseResult = new CommandLine(new LostAndFoundSwingApp()).parseArgs(
+				"--mongo-host=mongo.example",
+				"--mongo-port=12345",
+				"--db-name=lost-items",
+				"--db-collection=items");
+		assertThat(parseResult.matchedOptionValue("--mongo-host", "")).isEqualTo("mongo.example");
+		assertThat(parseResult.matchedOptionValue("--mongo-port", 0)).isEqualTo(12345);
+		assertThat(parseResult.matchedOptionValue("--db-name", "")).isEqualTo("lost-items");
+		assertThat(parseResult.matchedOptionValue("--db-collection", "")).isEqualTo("items");
+	}
 }
