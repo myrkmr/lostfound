d0c69bae8395b6a58a4bb626f4b8da3c9742564d
myrkmr <mayoorhablani@gmail.com>
2026-08-30 16:12:25 +0200

    support launcher Mongo configuration

--- src/java/com/examples/lostandfound/app/swing/LostAndFoundSwingApp.java
+++ src/java/com/examples/lostandfound/app/swing/LostAndFoundSwingApp.java
@@ -1,7 +1,20 @@
 package com.examples.lostandfound.app.swing;
 
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
 @Command(mixinStandardHelpOptions = true)
 public class LostAndFoundSwingApp {
+
+	@Option(names = "--mongo-host")
+	private String mongoHost = "localhost";
+
+	@Option(names = "--mongo-port")
+	private int mongoPort = 27017;
+
+	@Option(names = "--db-name")
+	private String databaseName = "lostfound";
+
+	@Option(names = "--db-collection")
+	private String collectionName = "lostitems";
 }
